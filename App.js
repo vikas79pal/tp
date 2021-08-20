7b7209dc8915c4b30fb6a2b19533bf02618da51c
vikas79pal <82100137+vikas79pal@users.noreply.github.com>
2021-08-20 14:28:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './InternshalComponents/Form'
+import "./tailwind.css"
+const App = () => {
+    return (
+        <div className={"bg-blue-500 w-screen h-screen flex justify-center items-center"}>
+            {/* Form */}
+            <Form/>
+            
+        </div>
+    )
+}
+
+export default App
---
+++ InternshalComponents/SignUp.js
@@ -0,0 +1,52 @@
+import React,{useState} from 'react'
+import "./SignUp.css"
+import Fb from "./facebook-770688_1920.png"
+import Google from "./logo-google-1991840_1920.png"
+import Li from "./linked-in-2668700_1280.png"
+import Ms from "./microsoftlogo.png"
+const SignUp = (props) => {
+    const changePageHandler=()=>{
+        props.onClick()
+    }
+    return (
+        <div className={" s_col items-center md:w-4/12    h-full  rounded-xl"}>
+            <h1 className={"font-bold text-xl lg:mt-4 md:mt-2"}>Sign Up</h1>
+            <form action="" className={"flex-col md:w-3/4 sm:w-full mt-4"}>
+               <label className={"block  w-11/12 sm:w-2/3 text-base lg:mt-1 md:mt-0"} htmlFor="name">Name</label>
+               <input className={"block outline-none rounded-sm h-7  lg:mt-1 md:mt-0 bg-blue-100 w-full "} type="text" name="name" id="name" />
+               <label className={"block  w-11/12 text-base lg:mt-1 md:mt-0"} htmlFor="email">Email</label>
+               <input className={"block outline-none rounded-sm h-7 lg:mt-1 md:mt-0 bg-blue-100 w-full "} type="email" name="email" id="email" />
+               <label className={"block  w-11/12 text-base lg:mt-1 md:mt-0 "} htmlFor="pass">Password</label>
+               <input className={"block outline-none rounded-sm h-7 lg:mt-1 md:mt-0 bg-blue-100 w-full "} type="password" name="pass" id="pass" />
+               <label className={"block  w-11/12 text-base lg:mt-1 md:mt-0 "} htmlFor="cpass">Confirm Password</label>
+               <input className={"block outline-none rounded-sm h-7 lg:mt-1 md:mt-0 bg-blue-100 w-full "} t type="password" name="cpass" id="cpass" />
+            <div className={"flex justify-center "}>
+
+            <button type="submit" className={"w-4/6 h-7 md:w-5/6 lg:w-1/3 md:h-6 lg:h-7 bg-blue-600 lg:mt-5 md:mt-2 mt-3 rounded-md text-gray-50 "}>Sign Up</button>
+            </div>
+            </form>
+
+<div className={"flex lg:mt-8 md:mt-3 w-full items-center mt-3  "}>
+
+            <hr className={" dash md:w-3/12 lg:w-1/3 w-3/12"} /> <p className={"lg:ml-2 md:ml-1 md:text-xs lg:text-xs  flex-grow md:mr-1 lg:mr-9 text-gray-400 text-xs"}>or connect with </p> <hr className={"w-3/12 md:w-3/12 lg:w-1/3 dash "} />
+</div>
+
+<div className={"flex lg:mt-8 md:mt-6 justify-around w-3/4 mt-3"}>
+<img src={Ms} className={" h-5 w-4/12 object-contain "} alt="sa" srcset="" />
+<img src={Fb} className={" h-5 w-4/12 object-contain "} alt="sa" srcset="" />
+<img src={Google} className={" h-5 w-4/12 object-contain "} alt="sa" srcset="" />
+<img src={Li} className={" h-5 w-4/12 object-contain "} alt="sa" srcset="" />
+</div>
+
+<div className={"lg:mt-12 md:mt-6 md:text-xs lg:text-sm flex justify-between w-full mt-5 text-xs"}>
+    <p className>Already have an Account?</p>
+    <p onClick={changePageHandler} className={"md:text-xs lg:text-sm text-xs md:text-black text-blue-500"}>Login </p>
+    
+    
+</div>
+
+        </div>
+    )
+}
+
+export default SignUp
